761069d1bef392f84fd31ec52caac9ad10bfab1b
Jozef Mikušinec <mik.jozef@gmail.com>
2019-09-23 12:38:54 +0200

    Update p5s-project.js

--- p5s-project.js
+++ p5s-project.js
@@ -5,8 +5,23 @@ const canvasSize = 400;
 
 const field = [];
 
+function initField() {
+  const fieldTmp = [];
+  
+  for (let x = 0; x < canvasSize; x++) {
+    fieldTmp[x] = [];
+    
+    for (let y = 0; y < canvasSize; y++) {
+      fieldTmp[x][y] = 0.1;
+    }
+  }
+  
+  field[0] = deepCopy(fieldTmp);
+  field[1] = deepCopy(fieldTmp);
+}
+
 function getColor(x) {
-  return Math.floor(low.map((a, i) => a + (high[i] - a) * x));
+  return low.map((a, i) => Math.floor(a + (high[i] - a) * x));
 }
 
 function setup() {
@@ -16,5 +31,16 @@ function setup() {
 }
 
 function draw() {
+  for (let x = 0; x < canvasSize; x++) {
+    for (let y = 0; y < canvasSize; y++) {
+      fill(...getColor(field[0][x][y]));
+      line(x, y, x, y);
+    }
+  }
+}
+
+function deepCopy(x) {
+  if (x.length) return x.map(a=> deepCopy(a));
   
+  return x;
 }
