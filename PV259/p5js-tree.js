81376a7d015abebf68018c5235e84d40c3b4c558
Jozef Mikušinec <mik.jozef@gmail.com>
2019-10-14 13:44:25 +0200

    Update p5js-tree.js

--- PV259/p5js-tree.js
+++ PV259/p5js-tree.js
@@ -7,6 +7,10 @@ const dirYMax =  10;
 const sizeMin = 30;
 const sizeMax = 50;
 
+const baseColor = [ 170, 150, 180 ];
+
+const extraBranchChance = 0.05;
+
 function rand(min, max) {
   return min + Math.floor(Math.random() * (max - min));
 }
@@ -21,33 +25,42 @@ class Tree {
     
     this.x = x;
     this.y = y;
+    this.color = color;
     this.size = size;
     this.dirX = dirX;
     this.dirY = dirY;
   }
   
-  growBranch(a) {
-    const x = this.x + this.size * Math.cos(a);
-    const y = this.y + this.size * Math.sin(a);
-    
-    this.children.push(new Tree(x, y));
+  growBranch(x, y) {
+    this.children.push(new Tree(x, y, color(...baseColor)));
   }
   
-  tick(delta) {
-    // TODO
+  tick(delta, mouseX, mouseY) {
+    this.children.forEach(a => a.tick(delta, mouseX, mouseY));
+    
+    if (this.children.length = 0) {
+      growBranch(this.x + this.dirX, this.y + this.dirY, color(...baseColor));
+      
+      if (Math.random() < extraBranchChance) growBranch();
+    }
     
     return this;
   }
   
-  draw(circle) {
-    circle(this.x, this.y, this.size)
+  draw(circle, stroke, fill) {
+    fill(this.color);
+    stroke(this.color);
+    
+    circle(this.x, this.y, this.size);
+    
+    this.children.forEach(a => a.draw(circle, stroke, fill));
   }
 }
 
 let root;
 
 function setup() {
-  root = new Tree(rand(0, 400), rand(0, 400), color(150));
+  root = new Tree(rand(0, 400), rand(0, 400), color(...baseColor));
   
   createCanvas(400, 400);
 }
@@ -55,5 +68,5 @@ function setup() {
 function draw() {
   background(255);
   
-  root.tick(deltaTime).draw(circle);
+  root.tick(deltaTime, mouseX, mouseY).draw(circle, stroke, fill);
 }
