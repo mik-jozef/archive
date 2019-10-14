20e8d1ddca45366711a5ca2aa5987fb0f9369496
Jozef Mikušinec <mik.jozef@gmail.com>
2019-10-14 12:56:11 +0200

    Update p5js-pieces.js

--- PV259/p5js-pieces.js
+++ PV259/p5js-pieces.js
@@ -1,6 +1,6 @@
 const pieces = 12;
 const size = 100;
-const tau = 2 * Math.PI;
+const pi = 2 * Math.PI;
 const period = 3000;
 const tide = 30;
 
@@ -17,9 +17,9 @@ function draw() {
   background(220);
   
   for (let i = 0; i < pieces; i++) {
-    const dx = Math.sin(tau * (time + i / pieces)) * Math.sin(time / 3) * 50;
-    const dy = Math.cos(tau * (time + i / pieces)) * Math.sin(time / 3) * 50;
+    const dx = Math.sin(pi * (time + i / pieces)) * Math.sin(time / 3) * 50;
+    const dy = Math.cos(pi * (time + i / pieces)) * Math.sin(time / 3) * 50;
     
-    arc(200 + dx, 200 + dy, size, size, tau * i / pieces, tau * (i + 1) / pieces);
+    arc(200 + dx, 200 + dy, size, size, pi * i / pieces, pi * (i + 1) / pieces);
   }
 }
