bf1365c45b7eff2fa81b64c327fea4fb99931fe9
Jozef Mikušinec <mik.jozef@gmail.com>
2019-10-14 13:26:32 +0200

    Update p5js-tree.js

--- PV259/p5js-tree.js
+++ PV259/p5js-tree.js
@@ -1,10 +1,29 @@
+const dirXMin = -10;
+const dirXMax =  10;
+
+const dirYMin = -10;
+const dirYMax =  10;
+
+const sizeMin = 30;
+const sizeMax = 50;
+
+function rand(min, max) {
+  return min + Math.floor(Math.random() * (max - min));
+}
+
 class Tree {
-  constructor(x, y, size = 0) {
+  constructor(x, y, color,
+    size = rand(sizeMin, sizeMax),
+    dirX = rand(dirXMin, dirXMax),
+    dirY = rand(dirYMin, dirYMax),
+  ) {
     this.children = [];
     
     this.x = x;
     this.y = y;
     this.size = size;
+    this.dirX = dirX;
+    this.dirY = dirY;
   }
   
   growBranch(a) {
@@ -14,15 +33,27 @@ class Tree {
     this.children.push(new Tree(x, y));
   }
   
-  tick() {
+  tick(delta) {
+    // TODO
     
+    return this;
+  }
+  
+  draw(circle) {
+    circle(this.x, this.y, this.size)
   }
 }
 
+let root;
+
 function setup() {
+  root = new Tree(rand(0, 400), rand(0, 400), color(150));
+  
   createCanvas(400, 400);
 }
 
 function draw() {
-  background(220);
+  background(255);
+  
+  root.tick(deltaTime).draw(circle);
 }
