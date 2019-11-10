52dc2f82742e1e6e7cf0d71ad732618a6b5c2fe7
Jozef Mikušinec <mik.jozef@gmail.com>
2019-11-10 22:21:45 +0100

    Update final.js

--- PV259/final.js
+++ PV259/final.js
@@ -2,63 +2,115 @@ const canvas = 400;
 const breakChange = 0.1;
 const breakChangeSpontaneous = 0.01;
 
-function rand(min, max) {
+const dimensions = 2; // 2 or 3
+
+function randN(min, max) {
   max === undefined && ([ min, max] = [ 0, min ]);
   
   return min + Math.floor(Math.random() * (max - min));
 }
 
-function randS(bound) {
+function randNS(bound) {
   return rand(1 - bound, bound);
 }
+
+function randHelper(f) {
+  return (...args) => {
+    const ret = [];
+    
+    for (let i = 0; i < dimensions; i++) {
+      ret.push(f(...args));
+    }
+    
+    return ret;
+  }
+}
+
+const [ rand, randS ] = [ randN, randNS ].map(randHelper);
 
 const asteroids = [];
 
 class Asteroid {
-  constructor(x, y) {
-    this.x = rand(canvas);
-    this.y = rand(canvas);
-    
-    this.dx = randS(10);
-    this.dy = randS(10);
-    
-    this.radius = ;
+  constructor(p, v, r) {
+    this.position = p;
+    this.velocity = v;
+    this.radius = 10;
   }
   
-  velocity() {
-    return Math.sqrt(this.dx ** 2 + this.dy ** 2);
+  velocityAbs() {
+    return Math.sqrt(this.velocity.map(a => a ** 2));
   }
   
   mass() {
-    return Math.PI * this.radius ** 2;
-  }
-  
-  momentum() {
-    return this.mass() * this.velocity();
-  }
-  
-  detectCollision() {
-    for (let asteroid of asteroids) {
-      if (this == asteroid) break;
+    switch (dimension) {
+      case 2: Math.PI * this.radius ** 2;
+      case 3: Math.PI * this.radius ** 3 * 4 / 3;
+      default: throw new Error("Dimension must be 2 or 3");
     }
   }
   
-  tick(dt) {
-    handleCollision()
+  momentum() {
+    return this.mass() * this.velocityAbs();
+  }
+  
+  detectCollision(iPtr) {
+    for (let asteroid of asteroids) {
+      if (this === asteroid) return false;
+      
+      // TODO
+    }
+  }
+  
+  move(dt) {
+    for (let i = 0; i < this.position.length; i++) {
+      this.position[i] += this.velocity[i] * dt;
+    }
+  }
+  
+  tick({ time, i0 = null, i1 = null }) {
+    if (i0 !== null) {
+      // TODO
+      
+      return;
+    }
+    
+    const collision = { time: null, i0: null, i1: null };
+    
+    if (asteroids.some(asteroid => asteroid.detectCollision(collision))) {
+      asteroids[i0].move(collision.time);
+      asteroids[i1].move(collision.time);
+      
+      this.tick(time - collision.time);
+      
+      return;
+    }
+    
+    for (let asteroid of asteroids) this.move(time);
   }
 }
 
 function setup() {
   createCanvas(400, 400);
   
-  asteroids.push(new Asteroid());
+  for (let i = randN(10) + 2; i > 0; i--) {
+    asteroids.push(new Asteroid(
+      rand(canvas),
+      randS(10),
+      randN(15, 35),
+    ));
+  }
 }
 
 function draw() {
   background(220);
   
   for (let asteroid of asteroids) {
-    circle(asteroid.x, asteroid.y, asteroid.radius, color(100, 100, 100));
+    circle(
+      asteroid.position[0],
+      asteroid.position[1],
+      asteroid.radius,
+      color(100, 100, 100)
+    );
     
     asteroid.tick(deltaTime);
   }
